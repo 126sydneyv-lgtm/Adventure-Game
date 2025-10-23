f1cb05dd3d9553fecfc2af969ef22c8cff1d1fc9
Sydney Valliere <126.sydney.v@gmail.com>
2025-10-23 11:28:18 -0400

    Add project files.

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.choice1Button = new System.Windows.Forms.Button();
+            this.choice2Button = new System.Windows.Forms.Button();
+            this.choice3Button = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // choice1Button
+            // 
+            this.choice1Button.Location = new System.Drawing.Point(186, 317);
+            this.choice1Button.Name = "choice1Button";
+            this.choice1Button.Size = new System.Drawing.Size(75, 23);
+            this.choice1Button.TabIndex = 0;
+            this.choice1Button.Text = "button1";
+            this.choice1Button.UseVisualStyleBackColor = true;
+            this.choice1Button.Click += new System.EventHandler(this.choice1Button_Click);
+            // 
+            // choice2Button
+            // 
+            this.choice2Button.Location = new System.Drawing.Point(382, 317);
+            this.choice2Button.Name = "choice2Button";
+            this.choice2Button.Size = new System.Drawing.Size(75, 23);
+            this.choice2Button.TabIndex = 1;
+            this.choice2Button.Text = "button1";
+            this.choice2Button.UseVisualStyleBackColor = true;
+            this.choice2Button.Click += new System.EventHandler(this.choice2Button_Click);
+            // 
+            // choice3Button
+            // 
+            this.choice3Button.Location = new System.Drawing.Point(574, 317);
+            this.choice3Button.Name = "choice3Button";
+            this.choice3Button.Size = new System.Drawing.Size(75, 23);
+            this.choice3Button.TabIndex = 2;
+            this.choice3Button.Text = "button1";
+            this.choice3Button.UseVisualStyleBackColor = true;
+            this.choice3Button.Click += new System.EventHandler(this.choice3Button_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(385, 114);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(44, 16);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.choice3Button);
+            this.Controls.Add(this.choice2Button);
+            this.Controls.Add(this.choice1Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button choice1Button;
+        private System.Windows.Forms.Button choice2Button;
+        private System.Windows.Forms.Button choice3Button;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        Random randGen = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayScene();
+        }
+
+        private void choice1Button_Click(object sender, EventArgs e)
+        {
+            HandleChoice(1);
+        }
+
+        private void choice2Button_Click(object sender, EventArgs e)
+        {
+            HandleChoice(2);
+        }
+
+        private void choice3Button_Click(object sender, EventArgs e)
+        {
+            HandleChoice(3);
+        }
+
+        private void HandleChoice(int choice)
+        {
+            switch (scene)
+            {
+                case 0: //check damage or oxygen
+                    if (choice == 1) scene = 1; //check damage
+                    else scene = 2; //check oxygen
+                    break;
+
+                case 1: //repair or seal the breach
+                    if (choice == 1) scene = 3; //repair 
+                    else scene = 4; //seal 
+                    break;
+
+                case 2: //control room or escape pod
+                    if (choice == 1) scene = 5; //control room
+                    else scene = 99;
+                    break;
+
+                case 3: //power choice
+                    if (choice == 1) scene = 5;
+                    else scene = 5;
+                    break;
+
+                case 4: //after sealing the breach
+                    scene = 5;
+                    break;
+
+                case 5: //cross coridor(random event)
+                    int chance = randGen.Next(1, 101);
+                    if (chance <= 70) scene = 6; //safe
+                    else scene = 7; //injured
+                    break;
+
+                case 6: //send signal to earth or colonies
+                    if (choice == 1) scene = 8; //earth
+                    else scene = 9; //colonies
+                    break;
+
+                case 7: //injured path
+                    if (choice == 1) scene = 8;
+                    else scene = 9;
+                    break;
+
+                case 8: //planet detected (land or orbit)
+                    if (choice == 1) scene = 10; //land
+                    else scene = 98; //rescued ending
+                    break;
+
+                case 9: // Planet detected from colony scan
+                    if (choice == 1) scene = 10;
+                    else scene = 98;
+                    break;
+
+                case 10: // Suit integrity before landing
+                    if (choice == 1) scene = 11; // fix suit
+                    else scene = 11; // risk it
+                    break;
+
+                case 11: //alien encounter (3 choices)
+                    if (choice == 1) scene = 91; // peaceful
+                    else if (choice == 2) scene = 92; // hide
+                    else scene = 93; // attack
+                    break;
+
+                default:
+                    Application.Exit();
+                    break;
+            }
+
+            DisplayScene();
+        }
+
+        private void DisplayScene()
+        {
+            choice3Button.Text = false;
+
+            switch (scene)
+            {
+                case 0:
+                    outputLabel.Text = "You wake up to flashing red lights. (Pause)\n“Warning: oxygen levels critical.”";
+                    choice1Button.Text = "Check damage";
+                    choice2Button.Text = "Check oxygen";
+                    break;
+
+                case 1:
+                    outputLabel.Text = "A hull breach alarm blares. Try to repair it or seal the door?";
+                    choice1Button.Text = "Repair breach";
+                    choice2Button.Text = "Seal the door";
+                    break;
+
+                case 2:
+                    outputLabel.Text = "Oxygen at 25%. Head for the control room or the escape pod?";
+                    choice1Button.Text = "Control room";
+                    choice2Button.Text = "Escape pod";
+                    break;
+
+                case 3:
+                    outputLabel.Text = "You repair the hull. You find the emergency generator sparking. Divert power?";
+                    choice1Button.Text = "Oxygen systems";
+                    choice2Button.Text = "Communications";
+                    break;
+
+                case 4:
+                    outputLabel.Text = "You seal off the breach and move toward the control room.";
+                    choice1Button.Text = "Continue";
+                    choice2Button.Text = "";
+                    break;
+
+                case 5:
+                    outputLabel.Text = "You must cross a damaged corridor to reach the control room...";
+                    choice1Button.Text = "Proceed";
+                    choice2Button.Text = "";
+                    break;
+
+                case 6:
+                    outputLabel.Text = "You make it safely. Send a signal to Earth or nearby colonies?";
+                    choice1Button.Text = "Earth";
+                    choice2Button.Text = "Colonies";
+                    break;
+
+                case 7:
+                    outputLabel.Text = "You’re injured crossing the corridor. Oxygen leaks faster. Send a signal?";
+                    choice1Button.Text = "Earth";
+                    choice2Button.Text = "Colonies";
+                    break;
+
+                case 8:
+                    outputLabel.Text = "Sensors detect a small livable planet nearby. Land or stay in orbit?";
+                    choice1Button.Text = "Land";
+                    choice2Button.Text = "Stay in orbit";
+                    break;
+
+                case 9:
+                    outputLabel.Text = "A livable planet shows up on your scan. Land or stay in orbit?";
+                    choice1Button.Text = "Land";
+                    choice2Button.Text = "Stay in orbit";
+                    break;
+
+                case 10:
+                    outputLabel.Text = "Your suit valve is loose. Fix it now or risk it during descent?";
+                    choice1Button.Text = "Fix it";
+                    choice2Button.Text = "Hope it holds";
+                    break;
+
+                case 11:
+                    outputLabel.Text = "You encounter alien lifeforms! What do you do?";
+                    choice1Button.Text = "Approach peacefully";
+                    choice2Button.Text = "Hide";
+                    choice3Button.Text = "Attack";
+                    choice3Button.Visible = true;
+                    break;
+
+                case 91:
+                    outputLabel.Text = "The aliens accept your peaceful approach. You form an alliance.";
+                    choice1Button.Text = "End Game";
+                    choice2Button.Text = "";
+                    break;
+
+                case 92:
+                    outputLabel.Text = "You try to hide, but the aliens find you. You’re captured.";
+                    choice1Button.Text = "End Game";
+                    choice2Button.Text = "";
+                    break;
+
+                case 93:
+                    outputLabel.Text = "You attack the aliens. They retaliate. You’re destroyed.";
+                    choice1Button.Text = "End Game";
+                    choice2Button.Text = "";
+                    break;
+
+                case 98:
+                    outputLabel.Text = "You stay in orbit. A rescue ship arrives. You’re saved!";
+                    choice1Button.Text = "End Game";
+                    choice2Button.Text = "";
+                    break;
+
+                case 99:
+                    outputLabel.Text = "You launch the escape pod but drift endlessly into space...";
+                    choice1Button.Text = "End Game";
+                    choice2Button.Text = "";
+                    break;
+            }
+
+        }
+    }
+}
